72e70a527cd5e083aac634c20b27d1defddfa315
Jose Laruta <eduardo.laruta@gmail.com>
2025-09-25 08:15:24 -0400

    fastapi 3

--- 2.tools/2.1.fastapi/hello_api.py
+++ 2.tools/2.1.fastapi/hello_api.py
@@ -12,6 +12,20 @@ def root():
 def saludo(nombre: str, edad: int):
     return {"message": f"hola {nombre}! mucho gusto en conocerte!"}
 
+@app.post("/estudiantes")
+def crear_estudiante(nombre: str, edad: int, curso: int):
+    estudiante_dic = {
+        "nombre": nombre, 
+        "edad": edad, 
+        "curso":curso
+    }
+    estudiantes.append(estudiante_dic)
+    return estudiante_dic
+
+@app.get("/estudiantes")
+def listar_estudiantes():
+    return estudiantes
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("hello_api:app", reload=True, port=8008)
