eef8916318e23ce1d580b25c7f5c88c08e9ebc20
Jose Laruta <eduardo.laruta@gmail.com>
2025-10-02 08:21:52 -0400

    cv deploy

---
+++ 3.computer_vision/3.3.deployment/3.3.1.object_detection_api/tools.py
@@ -0,0 +1,19 @@
+import io
+import numpy as np
+from fastapi import UploadFile, HTTPException, status
+from PIL import Image, UnidentifiedImageError
+
+def get_img_array(file: UploadFile) -> np.ndarray:
+    img_stream = io.BytesIO(file.file.read())
+
+    try:
+        img_obj = Image.open(img_stream)
+    except UnidentifiedImageError as e:
+        raise HTTPException(
+            status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE, 
+            detail=f"File not supported: {e}"
+            )
+    img_array = np.array(img_obj)
+    print(f"image array loaded: {img_array.shape}")
+    return img_array
+
